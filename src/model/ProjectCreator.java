223a3dfd7e42e981b3f786e36ccb42bec2aec1d1
hanch <jurco10510@gmail.com>
2020-12-16 18:33:06 +0100

    javadocs

--- src/model/ProjectCreator.java
+++ src/model/ProjectCreator.java
@@ -1,6 +1,13 @@
 package model;
 
 public class ProjectCreator extends Person{
+    /**
+     * Constructor of Developer which sets variables of Developer object
+     *
+     * @param name  name of ProjectCreator
+     * @param email email address of ProjectCreator
+     * @param role  role of Person which is in this case Developer can be changed to another role (ScrumMaster, ProjectCreator, ProjectOwner, Developer)
+     */
     public ProjectCreator(String name, String email, String role) {
         super(name, email, role);
     }
